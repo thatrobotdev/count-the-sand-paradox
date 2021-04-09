69813ea53c2eee6ba9d7bdf0f8fd558cb1ee941e
James Kerrane <hi@jameskerrane.com>
2021-04-09 14:56:08 -0600

    Hello World! 👋

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# count-the-sand-paradox
- A discord bot for people wanting to test the Sorites paradox by reverse counting sand in a heap.
+# count-sand-paradox
+
+THIS BOT IS A WORK-IN-PROGRESS, and it is not reccomended to add it to your server! Right now, this bot is probably broken, lacking features, or both!
+
+A discord bot for people wanting to test the [Sorites paradox](https://en.wikipedia.org/wiki/Sorites_paradox) by reverse counting sand in a heap.
+
+## The paradox
+
+Want to start an argument in your discord server? Here's one:
+
+    1,000,000 grains of sand is a heap of sand.
+    A heap of sand minus one grain is still a heap.
+
+Soo...
+
+    1,000,000 grains is a heap.
+    If 1,000,000 grains is a heap then 999,999 grains is a heap.
+    So 999,999 grains is a heap.
+    If 999,999 grains is a heap then 999,998 grains is a heap.
+    So 999,998 grains is a heap.
+    If ...
+    ... So 1 grain is a heap.
+
+But 1 grain isn't a heap! So... how many grains is a heap? Let discord decide!
+
+## Invite the bot
+
+<https://discord.com/oauth2/authorize?client_id=830102153556656129&scope=bot>
---
+++ index.js
@@ -0,0 +1,74 @@
+// Get arguments from .env
+require('dotenv').config();
+
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix } = require('./config.json');
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+client.cooldowns = new Discord.Collection();
+const commandFolders = fs.readdirSync('./commands');
+
+// Retrieve commands dynamically
+for (const folder of commandFolders) {
+	const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const command = require(`./commands/${folder}/${file}`);
+		client.commands.set(command.name, command);
+	}
+}
+
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// when the client sees a message
+client.on('message', (message) => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const commandName = args.shift().toLowerCase();
+
+	if (!client.commands.has(commandName)) return;
+
+	const command = client.commands.get((commandName));
+
+	// Cooldowns
+	const { cooldowns } = client;
+
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`Please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+		}
+	}
+
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+
+	try {
+		command.execute(message, args);
+	}
+	catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+
+});
+
+// login to Discord with your app's token
+client.login(process.env.TOKEN);
